d5cf8ac756c6d28c27efbeba8dace063f7f831dd
Sufferal <botnari_ciprian@mail.ru>
2024-03-01 07:46:10 +0200

    feat: add product increment to cart button

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
+// Navbar
+let counter = 0;
+const cartBtn = document.querySelector('#cartBtn');
+cartBtn.addEventListener('click', () => {
+  if (counter > 0) {
+    alert('Thank you for your purchase!');
+    counter = 0;
+    productCounter.textContent = counter;
+  }
+});
+
+// Products
+const productCounter = document.querySelector('#productCounter');
+const productBtns = document.querySelectorAll('.product-btn');
+
+productBtns.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    counter++;
+    productCounter.textContent = counter;
+  });
+});
+
+// Contact form
 const contactformElem = document.querySelector('#contactForm');
 const nameInputElem = document.querySelector('#name');
 const emailInputElem = document.querySelector('#email');
 const messageInputElem = document.querySelector('#message');
 const contactBtnElem = document.querySelector('#contactBtn');
 
-// Contact form
 contactBtnElem.addEventListener('click', (e) => {
   e.preventDefault();
 
